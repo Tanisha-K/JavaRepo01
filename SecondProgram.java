f658d00d4e9d0f6139a411f543ea341856ee1f52
Tanisha-K <tanishakothiyal@gmail.com>
2024-12-15 11:27:44 +0530

    Add files via upload

---
+++ SecondProgram.java
@@ -0,0 +1,10 @@
+class SecondProgram
+
+{
+  public static void main(String [] args)
+  { 
+  	System.out.println("Hello");
+  	System.out.println("Bye");
+  }
+
+}
---
+++ Solution.java
@@ -0,0 +1,11 @@
+class Solution
+{
+	public static void main(String [] args)
+	{
+
+	  System.out.print("Hello");
+	  System.out.println("How are you?");
+	  System.out.print("Im fine");
+	  System.out.println("Bye");
+	}
+}
